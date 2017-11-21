93a66a048889acebf7158022db9016d69c9da83a
Saurabh <309saurabhd@gmail.com>
2017-11-21 01:29:22 -0500

    beautify theme toggle implementation

--- src/components/codePane.js
+++ src/components/codePane.js
@@ -8,23 +8,8 @@ import Switch from 'react-toggle-switch';
 import "../../node_modules/react-toggle-switch/dist/css/switch.min.css"
 
 export default class CodePane extends Component {
-    toggleSwitch = () => {
-        this.setState(prevState => {
-            return {
-                switched: !prevState.switched
-            };
-        });
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            switched: false,
-        };
-    }
-
 	componentWillMount = () =>{
-		this.setState({codeData:""});
+		this.setState({codeData:"", switchTheme:false});
 	}
 
 	componentWillReceiveProps = (nextProps) => {
@@ -34,72 +19,42 @@ export default class CodePane extends Component {
 		})
 	}
 
-    nightMode() {
-        return (<div id = 'codePane' className = 'borderProps'>
-                <h4>Code</h4>
-                <Switch onClick={this.toggleSwitch} on={this.state.switched}/>
-                <hr/>
-                <AceEditor
-                    ref="ace"
-                    mode="java"
-                    theme="monokai"
-                    name="codeDisplay"
-                    value={this.state.codeData}
-                    fontSize={12}
-                    showPrintMargin={true}
-                    showGutter={true}
-                    highlightActiveLine={true}
-                    style={{
-                        width:"98%",
-                        height:"91%",
-                        "marginLeft": "1%",
-                        "borderRadius":"4px"
-                    }}
-                    setOptions={{
-                        enableBasicAutocompletion: false,
-                        enableLiveAutocompletion: false,
-                        enableSnippets: false,
-                        showLineNumbers: true,
-                    }}/>
-            </div>
-        );
-    }
-
-    dayMode() {
-        return (<div id = 'codePane' className = 'borderProps'>
-                <h4>Code</h4>
-                <Switch onClick={this.toggleSwitch} on={this.state.switched}/>
-                <hr/>
-                <AceEditor
-                    ref="ace"
-                    mode="java"
-                    theme="chrome"
-                    name="codeDisplay"
-                    value={this.state.codeData}
-                    fontSize={12}
-                    showPrintMargin={true}
-                    showGutter={true}
-                    highlightActiveLine={true}
-                    style={{
-                        width:"98%",
-                        height:"91%",
-                        "marginLeft": "1%",
-                        "borderRadius":"4px"
-                    }}
-                    setOptions={{
-                        enableBasicAutocompletion: false,
-                        enableLiveAutocompletion: false,
-                        enableSnippets: false,
-                        showLineNumbers: true,
-                    }}/>
-            </div>
-        );
-    }
+    toggleSwitch = () => {
+        this.setState(prevState => {
+            return {
+                switchTheme: !prevState.switchTheme
+            };
+        });
+    };
 
 	render() {
-        if(this.state.switched)
-            return this.nightMode();
-        else
-            return this.dayMode();
+        return (<div id = 'codePane' className = 'borderProps'>
+                    <h4>Code</h4>
+                    <Switch onClick={this.toggleSwitch} on={this.state.switchTheme}/>
+                    <hr/>
+                    <AceEditor
+                        ref="ace"
+                        mode="java"
+                        theme={this.state.switchTheme ? "chrome":"monokai"}
+                        name="codeDisplay"
+                        value={this.state.codeData}
+                        fontSize={12}
+                        showPrintMargin={true}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        style={{
+                            width:"98%",
+                            height:"91%",
+                            "marginLeft": "1%",
+                            "borderRadius":"4px"
+                        }}
+                        setOptions={{
+                            enableBasicAutocompletion: false,
+                            enableLiveAutocompletion: false,
+                            enableSnippets: false,
+                            showLineNumbers: true,
+                        }}/>
+                 </div>
+        );
 	}
 }
