ecdf916455aeb33a8b6687dbb2ed7d96c91b02f4
Ashutosh <ashutoshburde@gmail.com>
2017-11-20 23:16:04 -0500

    Added switch to toggle day and night mode

--- src/components/codePane.js
+++ src/components/codePane.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import brace from 'brace';
 import AceEditor from 'react-ace';
 import 'brace/mode/java';
-import 'brace/theme/monokai'
+import 'brace/theme/monokai';
+import 'brace/theme/chrome';
+import Switch from 'react-toggle-switch';
+import "../../node_modules/react-toggle-switch/dist/css/switch.min.css"
 
 export default class CodePane extends Component {
+    toggleSwitch = () => {
+        this.setState(prevState => {
+            return {
+                switched: !prevState.switched
+            };
+        });
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            switched: false,
+        };
+    }
+
 	componentWillMount = () =>{
 		this.setState({codeData:""});
 	}
@@ -16,31 +34,72 @@ export default class CodePane extends Component {
 		})
 	}
 
+    nightMode() {
+        return (<div id = 'codePane' className = 'borderProps'>
+                <h4>Code</h4>
+                <Switch onClick={this.toggleSwitch} on={this.state.switched}/>
+                <hr/>
+                <AceEditor
+                    ref="ace"
+                    mode="java"
+                    theme="monokai"
+                    name="codeDisplay"
+                    value={this.state.codeData}
+                    fontSize={12}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    style={{
+                        width:"98%",
+                        height:"91%",
+                        "marginLeft": "1%",
+                        "borderRadius":"4px"
+                    }}
+                    setOptions={{
+                        enableBasicAutocompletion: false,
+                        enableLiveAutocompletion: false,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                    }}/>
+            </div>
+        );
+    }
+
+    dayMode() {
+        return (<div id = 'codePane' className = 'borderProps'>
+                <h4>Code</h4>
+                <Switch onClick={this.toggleSwitch} on={this.state.switched}/>
+                <hr/>
+                <AceEditor
+                    ref="ace"
+                    mode="java"
+                    theme="chrome"
+                    name="codeDisplay"
+                    value={this.state.codeData}
+                    fontSize={12}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    style={{
+                        width:"98%",
+                        height:"91%",
+                        "marginLeft": "1%",
+                        "borderRadius":"4px"
+                    }}
+                    setOptions={{
+                        enableBasicAutocompletion: false,
+                        enableLiveAutocompletion: false,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                    }}/>
+            </div>
+        );
+    }
+
 	render() {
-		return (<div id = 'codePane' className = 'borderProps'>
-			      <h4>Code</h4>
-				  <hr/>				
-				  <AceEditor
-					  mode="java"
-					  theme="monokai"
-					  name="codeDisplay"
-					  value={this.state.codeData}
-					  fontSize={12}
-					  showPrintMargin={true}
-					  showGutter={true}
-					  highlightActiveLine={true}	
-					  style={{
-					  	width:"98%",
-					  	height:"91%",
-    					"marginLeft": "1%",
-    					"borderRadius":"4px"
-					  }}				  
-					  setOptions={{
-					  enableBasicAutocompletion: false,
-					  enableLiveAutocompletion: false,
-					  enableSnippets: false,
-					  showLineNumbers: true,
-					  }}/>
-	    		</div>)
+        if(this.state.switched)
+            return this.nightMode();
+        else
+            return this.dayMode();
 	}
 }
