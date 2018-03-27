2276146646d5718a612eba4a132128f988668d92
RaghavRaman <rghv.raman@gmail.com>
2018-03-26 23:09:48 -0400

    Conflict resolved

--- apis/addGraders.php
+++ apis/addGraders.php
@@ -1,48 +1,25 @@
 <?php
-include './defaults.php';
+include 'defaults.php';
 header("Content-Type: application/json; charset=UTF-8");
 
-$data = $_POST['data'];
+$data = $_POST['info'];
 $data = json_decode($data);
 $graders = $data->graders;
-<<<<<<< HEAD
-print_r($data);
-=======
->>>>>>> c26710b678e7894764b03d10d0431d97688d12f8
-// $course_id = $data->course_id;
+$course_id = $data->course_id;
 $course_code = strtoupper(trim($data->course_code));
 $sec_code = strtoupper(trim($data->sec_code));
 $term = strtoupper(trim($data->term));
-<<<<<<< HEAD
-$type = strtoupper(trim($data->type));
-
-if(is_null($course_code) || is_null($sec_code) || is_null($term) || is_null($type)){
-=======
-// $course_code = "2710 ADVANCED DATABASE";
-// $sec_code = "2001";
-// $term = "SPRING 2018";
+// $course_code = "2710 ADVANCE DATABASE";
+// $sec_code = "2006";
+// $term = "SPRING 2019";
 // $graders = array();
 // array_push($graders, array('name'=>"raghav","email"=>"rar155"));
 
 if(is_null($course_code) || is_null($sec_code) || is_null($term)){
->>>>>>> c26710b678e7894764b03d10d0431d97688d12f8
      $response = array('code' => 400, 'message' => 'Missing values in parameters','error'=> 'missing values');
     $response = json_encode($response);
     echo $response;
 }else{
-<<<<<<< HEAD
-    $courseId = getCourseCode($course_code,$sec_code,$term,$mysqli);
-    $filename = "./filename.txt";
-    if($stmt = $mysqli->prepare("INSERT INTO grader(id, name, course_id) VALUES (?, ?, ?,?)")){
-
-        $stmt->bind_param("ssss", $email, $name, $course_id);
-        
-        $errorMessage = null;
-
-        $SQLRecordsFailed = array();
-        $FileRecordsFailed = array();
-
-=======
     $course_id = getCourseCode($course_code,$sec_code,$term,$mysqli);
     if($course_id == null){
         $response = array('code' => 400, 'message' => 'No Course Found');
@@ -55,10 +32,11 @@ if(is_null($course_code) || is_null($sec_code) || is_null($term)){
         $stmt->bind_param("sss", $email, $name, $course_id);
         
         $errorMessage = null;
+        echo $graders;
         $SQLRecordsFailed = array();
         $FileRecordsFailed = array();
->>>>>>> c26710b678e7894764b03d10d0431d97688d12f8
         foreach ($graders as $tuple) {
+            echo "string";
             $name = $tuple->name;
             $email = $tuple->email;
 
@@ -73,10 +51,6 @@ if(is_null($course_code) || is_null($sec_code) || is_null($term)){
                 }
             }
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> c26710b678e7894764b03d10d0431d97688d12f8
         if ((count($SQLRecordsFailed) == 0) && (count($FileRecordsFailed) == 0)) {
             $response = array('code' => 200, 'message' => 'Success');
             $response = json_encode($response);
@@ -86,27 +60,11 @@ if(is_null($course_code) || is_null($sec_code) || is_null($term)){
             $response = json_encode($response);
             echo $response;
         }
-<<<<<<< HEAD
-
-
-        if ($stmt)
-            $stmt->close();
-
-    }else{
-        $response = array('code' => 400, 'message' => 'Error Ading Graders','error'=> $stmt->error);
-        $response = json_encode($response);
-        echo $response;
-
-    }
-    
-
-=======
         if ($stmt)
             $stmt->close();
     
     }
     
->>>>>>> c26710b678e7894764b03d10d0431d97688d12f8
 }
 $mysqli->close();
 ?>
